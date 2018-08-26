ff597afb4241a7ffb6b84db3a92f15a100e92a97
sigavpn <34906324+sigavpn@users.noreply.github.com>
2018-08-26 18:40:53 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>IP Leak Test</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap.min.css">
+  <script src="jquery.min.js"></script>
+  <script src="bootstrap.min.js"></script>
+<?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
+</head>
+<body>
+<div class="jumbotron">
+  <div class="container text-center">
+    <h1>IP Information</h1>      
+    <p>
+<?php 
+switch ($_SERVER["HTTP_CF_CONNECTING_IP"]) {
+    case "216.245.220.14":
+        echo "You are connected to SigaVPN";
+        break;
+    case "185.61.149.116":
+        echo "You are connected to SigaVPN";
+        break;
+    case "142.44.133.83":
+        echo "You are connected to SigaVPN";
+        break;
+    case "51.15.2.172":
+        echo "You are connected to SigaVPN";
+        break;
+    case "95.215.44.147":
+        echo "You are connected to SigaVPN";
+        break;
+    case "185.225.17.101":
+        echo "You are connected to SigaVPN";
+        break;
+    case "205.185.122.55":
+        echo "You are connected to SigaVPN";
+        break;
+    default:
+        echo "<b><font color='red'>You are not connected to SigaVPN</font></b>";
+}
+?>
+</p>
+  </div>
+</div>
+<div class="container-fluid bg-3 text-center">    
+  <div class="row">
+    <div class="col-sm-3">
+      <p><b>IP Address</b></p>
+	<p><?php echo $_SERVER["HTTP_CF_CONNECTING_IP"]; ?></p>
+    </div>
+    <div class="col-sm-3"> 
+      <p><b>Country</b></p>
+        <p><?php echo $_SERVER["HTTP_CF_IPCOUNTRY"]; ?></p>    
+</div>
+    <div class="col-sm-3"> 
+      <p><b>User Agent</b></p>
+        <p><?php echo $_SERVER['HTTP_USER_AGENT']; ?>
+</p>    
+</div>
+    <div class="col-sm-3">
+      <p><b>ISP</b></p>
+	<p>
+<?php
+$ipaddress = $_SERVER["HTTP_CF_CONNECTING_IP"];
+function ip_details($ip) {
+    $json = file_get_contents("http://ipinfo.io/{$ip}/json");
+    $details = json_decode($json); 
+    return $details;
+}
+$details = ip_details($ipaddress);
+echo $details->org;
+?>
+</p>
+    </div>
+  </div>
+</div><br>
+</body>
+</html>
+
