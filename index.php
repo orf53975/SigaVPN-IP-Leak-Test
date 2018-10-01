051963ae2ce5db13d9f2db351b0de295e264f261
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2018-10-01 11:48:24 +0100

    Added extra header to prevent caching.

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 <?php
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
+header("Expires: 0");
 header("Pragma: no-cache");
 ?>
 </head>
