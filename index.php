8af96ebe69f0a7b6cbf032802d4f4b2418efabb8
sigavpn <34906324+sigavpn@users.noreply.github.com>
2018-10-23 09:04:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -18,23 +18,6 @@ header("Pragma: no-cache");
 <div class="jumbotron">
   <div class="container text-center">
     <h1>IP Information</h1>      
-    <p>
-<?php 
-switch ($_SERVER["HTTP_CF_CONNECTING_IP"]) {
-    case "216.245.220.14":
-    case "185.61.149.116":
-    case "142.44.133.83":
-    case "51.15.2.172":
-    case "95.215.44.147":
-    case "185.225.17.101":
-    case "205.185.122.55":
-        echo "You are connected to SigaVPN";
-        break;
-    default:
-        echo "<b><font color='red'>You are not connected to SigaVPN</font></b>";
-}
-?>
-</p>
   </div>
 </div>
 <div class="container-fluid bg-3 text-center">    
